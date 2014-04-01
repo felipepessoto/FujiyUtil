9d80783a657f3c27d39e769b448ba81d6011de81
Fujiy <felipepessoto@gmail.com>
2014-04-01 10:09:39 -0300

    CLSCompliant

--- src/Fujiy.Util/Caching/CacheHelper.cs
+++ src/Fujiy.Util/Caching/CacheHelper.cs
@@ -20,25 +20,21 @@ namespace Fujiy.Util.Caching
         private static bool cacheEnabled = true;
         public static bool CacheEnabled { get { return cacheEnabled; } set { cacheEnabled = value; } }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "Não há outra técnica para isto. E não aumenta a complexidade já que a expression é um syntactic sugar, o cliente apenas escreve um lambda")]
         public static TResult FromCacheOrExecute<TResult>(Expression<Func<TResult>> func)
         {
             return FromCacheOrExecute(func, null, null);
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "Não há outra técnica para isto. E não aumenta a complexidade já que a expression é um syntactic sugar, o cliente apenas escreve um lambda")]
         public static TResult FromCacheOrExecute<TResult>(Expression<Func<TResult>> func, string key)
         {
             return FromCacheOrExecute(func, key, null);
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "Não há outra técnica para isto. E não aumenta a complexidade já que a expression é um syntactic sugar, o cliente apenas escreve um lambda")]
         public static TResult FromCacheOrExecute<TResult>(Expression<Func<TResult>> func, CacheItemPolicy cacheItemPolicy)
         {
             return FromCacheOrExecute(func, null, cacheItemPolicy);
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "MethodCallExpression"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures", Justification = "Não há outra técnica para isto. E não aumenta a complexidade já que a expression é um syntactic sugar, o cliente apenas escreve um lambda")]
         public static TResult FromCacheOrExecute<TResult>(Expression<Func<TResult>> func, string key, CacheItemPolicy cacheItemPolicy)
         {
             if (func == null)
@@ -128,14 +124,11 @@ namespace Fujiy.Util.Caching
 
         public static void RemoveCache<TResult>(Expression<Func<TResult>> func)
         {
-            MethodCallExpression method = func.Body as MethodCallExpression;
+            if (func == null)
+                throw new ArgumentNullException("func");
 
-            if (method == null)
-            {
-                throw new InvalidCachedFuncException("Body must be MethodCallExpression to auto generate a cache key");
-            }
+            string key = ExtractKeyFromExpression(func);
 
-            string key = CacheKeyGenerator.GenerateKey(method);
             DefaultCache.Remove(key);
         }
 
@@ -162,7 +155,6 @@ namespace Fujiy.Util.Caching
 
         #region Groups
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "O método pode demorar caso tenha muitas chaves, além de poder gerar resultados diferentes em duas chamadas consecutivas.")]
         public static ILookup<string, string> GetAllKeys()
         {
             lock (KeysGroups)
